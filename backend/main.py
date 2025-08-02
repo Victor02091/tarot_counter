d18497b066d2b0f96c747a0457cb5db72b596d5f
Victor Osso <osso.victor@orange.fr>
2025-08-01 23:04:58 -0600

    feat: test connection with postgres db

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,12 @@
-def main():
-    print("Hello from backend!")
+from sqlalchemy import create_engine, text
 
+def main():
+    DATABASE_URL = "postgresql+psycopg2://victor_user:victor_password@localhost:5432/tarot_db"
+    engine = create_engine(DATABASE_URL)
+
+    with engine.connect() as conn:
+        result = conn.execute(text("SELECT NOW();"))
+        print(result.fetchone())
 
 if __name__ == "__main__":
     main()
