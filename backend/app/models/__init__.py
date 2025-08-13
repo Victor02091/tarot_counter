92268f5e9b8785a46bce3bb7d7508fea0cd02a21
Victor Osso <osso.victor@orange.fr>
2025-08-13 14:33:31 +0200

    feat: auto import new models for alembic detection

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,4 +1,8 @@
-from app.models.questionary import (
-    Questionnaire,
-)  # this triggers model registration
-from app.models.party_results import PartyResult
+import importlib
+import pkgutil
+import pathlib
+
+# dynamically import all submodules in the models package for alembic to detect
+package_dir = pathlib.Path(__file__).parent
+for (_, module_name, _) in pkgutil.iter_modules([str(package_dir)]):
+    importlib.import_module(f"{__name__}.{module_name}")
